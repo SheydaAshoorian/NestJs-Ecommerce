d69761f624aeff00ee852035e5f1859d37346aed
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-24 12:29:58 +0330

    set transform-validator config in makin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // transfor-validator config 
+  app.useGlobalPipes(new ValidationPipe({
+  whitelist: true, // فیلدهای اضافه را حذف می‌کند
+  transform: true, // جادوی تبدیل نوع را فعال می‌کند
+  transformOptions: {
+    enableImplicitConversion: true, // تبدیل هوشمند انواع ساده
+  },
+}));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
