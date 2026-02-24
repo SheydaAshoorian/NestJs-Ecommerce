4c198bad3a5047f63fd1fe1565d683f4cd2f9b77
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-24 14:57:01 +0330

    add product entity and active interceptor for Serializer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe,ClassSerializerInterceptor }  from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // transfor-validator config 
+  //**************** transfor-validator config  ********************** */ 
+
   app.useGlobalPipes(new ValidationPipe({
   whitelist: true, // فیلدهای اضافه را حذف می‌کند
   transform: true, // جادوی تبدیل نوع را فعال می‌کند
@@ -13,6 +15,14 @@ async function bootstrap() {
   },
 }));
 
+
+//***************************** فعال‌سازی اینترسپتور به صورت سراسری********************************* */
+  // اول باید Reflector رو بگیریم چون اینترسپتور برای خوندن دکوراتورها بهش نیاز داره
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+
+  //************* تنظیم پورت یک اند************ */
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,21 @@
-export class Product {}
+import { Product } from "@prisma/client";  // استفاده از تایپ خودکار پریزما برای امنیت کُد
+
+
+export class ProductEntity implements Product{
+
+    constructor(partial: Partial<ProductEntity>) {
+        Object.assign(this, partial);
+    }
+
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    shop_id: number;
+    category_id: number;
+    seller_id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date | null; // دقت کنید که در پریزما معمولاً فیلدهای ح
+
+}
