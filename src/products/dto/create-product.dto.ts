e3c6f7ed61244303065043be57ea97d1fe46d2aa
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-24 10:33:45 +0330

    add product dto and swagger plugin  config in nest-cli.json

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,32 @@
-
+import { IsString,IsNumber, IsNotEmpty,IsInt,IsOptional,IsPositive } from 'class-validator';
+import { Type } from 'class-transformer';  
 
 export class CreateProductDto {
 
-    
+    @IsString()
+    @IsNotEmpty()
+    name:string;
+
+    @IsString()
+    @IsOptional()
+    description :  string;
+
+    @IsNumber({maxDecimalPlaces:2 })
+    @IsPositive()
+    @Type(()=>Number) // تبدیل استرینگ به عدد
+    price: number;
+
+    @IsInt()
+    @Type(()=>Number)
+    shop_id: number;
+
+    @IsInt()
+    @Type(()=>Number)
+    category_id: number;
+
+    @IsInt()
+    @Type(()=>Number)
+    seller_id :  number;
 
 }
+
