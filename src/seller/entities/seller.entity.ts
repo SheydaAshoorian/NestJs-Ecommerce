5e70544ba11ee1ab03c779a2cccd225f6ae3f09d
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-26 09:47:10 +0330

    add seller entity

--- src/seller/entities/seller.entity.ts
+++ src/seller/entities/seller.entity.ts
@@ -1,1 +1,24 @@
-export class Seller {}
+import { Seller } from '@prisma/client';
+import { ShopEntity } from '@shops/entities/shop.entity';
+import { ProductEntity } from '@products/entities/product.entity';
+
+export class SellerEntity implements Seller {
+
+  id : number;        
+  first_name: string;
+  last_name:  string;
+  email:      string;  
+  phone:      string;  
+  shops:      ShopEntity[];
+  createdAt:  Date;
+  updatedAt:  Date;
+  deletedAt:  Date | null;
+
+
+  constructor (partial: Partial<SellerEntity>){
+
+    Object.assign(this,partial)
+
+  }
+
+}
--- src/shop/dto/create-shop.dto.ts
+++ src/shop/dto/create-shop.dto.ts
@@ -1,15 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
 import { ProductEntity } from '@products/entities/product.entity';
 
-
-export class CreateShopDto{
-
+export class CreateShopDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   address: string;
+
+  @IsInt()
+  @Type(() => Number)
+  @IsNotEmpty()
   seller_id: number;
-  products?: ProductEntity[];
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date | null;
 
 }
